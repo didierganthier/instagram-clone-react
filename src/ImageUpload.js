ef2781d188bd08911857cbfd2c7044b17b0323a4
didierganthier <didierganthierperan@gmail.com>
2021-02-09 15:18:51 -0500

    selected file reference showing

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Button } from '@material-ui/core'
+import React, { useState } from 'react'
 
 function ImageUpload() {
+    const [image, setImage] = useState(null);
+    const [progress, setProgress] = useState(0);
+    const [caption, setCaption] = useState('');
+
+    const handleChange = (e) => {
+        if(e.target.files[0]){
+            setImage(e.target.files[0]);
+        }
+    }
+
+    const handleUpload = () => {
+
+    }
+
     return (
         <div>
-            
+            <input type="text" placeholder="Enter a caption..." onChange={event => setCaption(event.target.value)} value={caption}/>
+            <input type="file" onChange={handleChange}/>
+            <Button onClick={handleUpload}>
+                Upload
+            </Button>
         </div>
     )
 }
