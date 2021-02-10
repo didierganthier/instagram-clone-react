5c7537bef07d81f055b376b6a85e164e24399d32
didierganthier <didierganthierperan@gmail.com>
2021-02-10 15:34:52 -0500

    input field fpr comment UI

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,33 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import { useState, useEffect } from "react";
+import { db } from './firebase';
 import './Post.css'
 
-function Post({ username, caption, imageUrl }) {
+function Post({ username, caption, imageUrl, key, postId }) {
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
+
+    useEffect(() => {
+      let unsubscribe;
+      if(postId) {
+        unsubscribe = db
+          .collection("posts")
+          .doc(postId)
+          .collection("comments")
+          .onSnapshot((snapshot) => {
+            setComments(snapshot.docs.map((doc) => doc.data()));
+          });
+      }
+      return () => {
+        unsubscribe();
+      }
+    }, [postId])
+
+    const postComment = (event) => {
+
+    }
+
     return (
         <div className="post">
             <div className="post__header">
@@ -21,6 +46,24 @@ function Post({ username, caption, imageUrl }) {
             />
 
             <h4 className="post__text"><strong>{username}: </strong>{caption}</h4>
+
+            <form className="post__commentBox">
+              <input
+                className="post__input"
+                type="text"
+                placeholder="Add a comment..."
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              />
+              <button
+                className="post__button"
+                disabled={!comment}
+                type="submit"
+                onClick={postComment}
+              >
+                Post
+              </button>
+            </form>
         </div>
     );
 }
